25c9055d0e50a717c8a3e954f819d37865a12e41
Christian <caldanas2@miumg.edu.gt>
2022-03-19 10:18:03 -0600

    FUNCION SUMAR

---
+++ src/calculadora/CALCULADORA.java
@@ -0,0 +1,15 @@
+
+package calculadora;
+
+/**
+ *
+ * @author Cristhian
+ */
+public class CALCULADORA {
+
+
+    public static void main(String[] args) {
+        
+        //atributos.
+        
+    }
---
+++ src/calculadora/CALCULADORA1.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Cristhian
+ */
+public class CALCULADORA1 {
+       private int Num_001;
+         private int Num_002;
+        
+      //inicia el proceso de constructores.
+      
+    public CALCULADORA1(){
+        
+    }
+    public CALCULADORA1 (int Num_001, int Num_002){
+        
+        this.Num_001 = Num_001;
+        this.Num_002 = Num_002;
+        
+      //Inicia el metodo
+      
+    
+    }
+    //Aqui inicia los metodos.
+    public int sumar(){
+        int suma = this.Num_001 + this.Num_002;
+        return suma;
+    }
+
+    
+    
+    
+    
+    
+    public int getNum_001() {
+        return Num_001;
+    }
+
+    public void setNum_001(int Num_001) {
+        this.Num_001 = Num_001;
+    }
+
+    public int getNum_002() {
+        return Num_002;
+    }
+
+    public void setNum_002(int Num_002) {
+        this.Num_002 = Num_002;
+    }
+    
+}
